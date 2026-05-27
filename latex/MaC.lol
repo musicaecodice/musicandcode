04d44e814a196d42c1ab43f69b1781ba550eb518
anvigani <13Vivaldi70>
2026-05-27 17:46:33 +0200

    init

---
+++ latex/MaC.lol
@@ -0,0 +1,3 @@
+\contentsline {lstlisting}{\numberline {2.1}Instrument model}{38}{lstlisting.2.1}%
+\contentsline {lstlisting}{\numberline {2.2}Dynamic voice allocation}{39}{lstlisting.2.2}%
+\contentsline {lstlisting}{\numberline {2.3}Static voice allocation}{39}{lstlisting.2.3}%
---
+++ on_server/media/6_media/index.php
@@ -0,0 +1,134 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title>Musica e Codice</title>
+	    <link rel="stylesheet" type="text/css" href="stile.css">
+	</head>		
+	<body>
+		<header id="header">
+			<div id="logo">
+				<font size=4>Andrea Vigani</font>
+				<h1>Musica e Codice</h1>
+				<p><font color="black">Appunti di informatica musicale</font></p>
+			</div>
+		</header>
+		
+		<main id="main">
+			<div class="innertube">				
+                <!--<p><a name="appunti"><h3>Appunti Musicali</h3></a></p>
+				<ul>
+			        <!--<li><a href="Premessa/Premessa.php"><b>Premessa</b></a></li>-->
+				    <!--<li><a href="Appunti_musicali/Appunti_musica.php"><b>Frasi sparse</b></a></li>-->
+				    <!--<li><a href="Ascoltare/Ascoltare.php"><b>Tipologie d'ascolto</b></a></li>-->
+				    <!--<li><a href="Ascolti/Ascolti.php"><b>Ascolti</b></a></li>-->
+				    <!--<li><a href="Parametri_suono/Parametri_suono.php"><b>Parametri del suono e della musica</b></a></li>-->
+				    <!--<li><a href="Misure_tempo/Misure_tempo.php"><b>Misurare il tempo</b></a></li>-->
+				    <!--<li><a href="Articolare_tempo/Articolare_tempo.php"><b>Articolare il tempo</b></a></li>-->
+				    <!--<li><a href="Leggere_suono/Leggere_suono.php"><b>Leggere il suono</b></a></li>-->
+                    <!--<li><a href="Determinismo/Determinismo.php"><b>Determinismo</b></a></li>-->
+				    <!--<li><a href="Indeterminismo/Indeterminismo.php"><b>Indeterminismo</b></a></li>-->
+				    <!--<li><a href="Processi/Processi.php"><b>Due processi creativi</b></a></li>-->
+                </ul>
+                <p><a name="amb"><h3>Ambienti esecutivi</h3></a></p>
+                <ul>
+				    <li><a href="Catene/Catene.php"><b>Catene elettroacustiche</b></a></li>			
+                    <li><a href="Strumento/Strumento.php"><b>Il computer come strumento musicale</b></a></li>
+                    <li><a href="Live_electronics/Live_electronics.php"><b>Tipologie di live electronics</b></a></li>
+				</ul>
+                <p><a name="tec"><h3>Appunti teorico informatici</h3></a></p>
+                <ul>
+                    <li><a href="Linguaggi_codici/Linguaggi_codici.php"><b>Linguaggi e codici</b></a></li>
+				    <li><a href="Apprendimento/Apprendimento.php"><b>Apprendimento <small>(I'm sitting in a room)</small></b></a></li>
+                    <li><a href="Elettricita_circuiti/El_Circ.php"><b>Elettricità e circuiti</b></a></li>   
+                    <!--<li><a href="Segnali/Segnali.php"><b>Campionamento del segnale</b></a></li>-->
+                    <!--<li><a href="Procedure/Procedure.php"><b>Procedure e schemi a blocchi</b></a></li>-->
+                </ul>
+                    <h3><a name="SC">SuperCollider</a></h3>
+				<ul>
+					<!--<li><a href="SC_Intro/SC_Intro.php"><b>Intro</b></a></li>-->
+					<li><a href="SC_Elementi/SC_Elementi.php"><b>Elementi del linguaggio</b></a></li>
+					<li><a href="SC_OOP/SC_OOP.php"><b>Programmazione Orientata ad Oggetti</b></a></li>
+					<!--<li><a href="SC_Server/SC_Server.php"><b>Server</b></a></li>-->
+					<!--<li><a href="SC_UGens/SC_UGens.php"><b>UGens, Synth e segnali</b></a></li>-->
+					<li><a href="SC_Env/SC_Env.php"><b>Rampe e inviluppi</b></a></li>
+					<li><a href="SC_Controllo/SC_Controllo.php"><b>Controllo dei parametri e strategie</b></a></li>
+					<ul>
+						<li><a href="SC_Controllo/SC_Valori/SC_Valori.php"><b>Singoli valori e smoothing</b></a></li>
+						<li><a href="SC_Controllo/SC_Sequencing/SC_Sequencing.php"><b>Sequencing</b></a></li>
+						<li><a href="SC_Controllo/SC_Pattern/SC_Pattern.php"><b>Pattern</b></a></li>
+						<li><a href="SC_Controllo/SC_Gui/SC_Gui.php"><b>GUI</b></a></li>
+						<li><a href="SC_Controllo/SC_Mouse/SC_Mouse.php"><b>Mouse</b></a></li>
+						<li><a href="SC_Controllo/SC_Tastiera/SC_Tastiera.php"><b>Tastiera</b></a></li>
+						<li><a href="SC_Controllo/SC_Midi/SC_Midi.php"><b>MIDI</b></a></li>
+						<li><a href="SC_Controllo/SC_Osc/SC_Osc.php"><b>OSC</b></a></li>
+						<li><a href="SC_Controllo/SC_Segnali/SC_Segnali.php"><b>Segnali di controllo</b></a></li>
+					</ul></br>
+					<li><a href="SC_Panning/SC_Panning.php"><b>Panning</b></a></li>
+                    <ul>
+						<li><a href="SC_Panning/SC_Mono/SC_Mono.php"><b>Monofonia</b></a></li>
+						<li><a href="SC_Panning/SC_Stereo/SC_Stereo.php"><b>Stereofonia</b></a></li>
+						<li><a href="SC_Panning/SC_Quadri/SC_Quadri.php"><b>Quadrifonia</b></a></li>
+                        <li><a href="SC_Panning/SC_Circola/SC_Circola.php"><b>Sistemi circolari</b></a></li>
+                        <li><a href="SC_Panning/SC_Dedica/SC_Dedica.php"><b>Sistemi dedicati</b></a></li>
+					</ul></br>
+					<li><a href="SC_Memorie/SC_Buffers.php"><b>Memorie</b></a></li>
+					<ul>
+						<li><a href="SC_Memorie/SC_Soundfiles/SC_Soundfiles.php"><b>Soundfiles</b></a></li>
+						<li><a href="SC_Memorie/SC_Live/SC_Live.php"><b>Live input</b></a></li>
+						<li><a href="SC_Memorie/SC_Wavetables/SC_Wavetables.php"><b>Wavetables</b></a></li>
+					</ul></br>
+					<li><a href="SC_Playback/SC_Playback.php"><b>Playback</b></a></li>
+					<ul>
+						<li><a href="SC_Playback/SC_Players/SC_Players.php"><b>Players</b></a></li>
+						<li><a href="SC_Playback/SC_Elaboratori/SC_Elaboratori.php"><b>Elaboratori</b></a></li>
+					    <li><a href="SC_Playback/SC_Oscillatori/SC_Oscillatori.php"><b>Oscillatori tabellari</b></a></li>
+<!--			
+					    <li><a href="SC_Granulare/SC_Granulare.php"><b>Sintesi granulare [...]</b></a></li>
+					    <li><a href="SC_Concatenativa/SC_Concatenativa.php"><b>Sintesi concatenativa [...]</b></a></li>
+-->	
+					</ul></br>		
+                    <li><a href="SC_Sint_vett/SC_Sint_vett.php"><b>Crossfading e sintesi vettoriale</b></a></li>
+                    <li><a href="SC_Timb_stratch/SC_Timb_stratch.php"><b>Timbre stretching [...]</b></a></li>
+				</ul>
+				<h3><a name="arduino">Arduino</a></h3>
+				<ul>
+					<li><a href="AR_Intro/AR_Intro.php"><b>Intro</b></a></li>
+					<li><a href="AR_Circuiti/AR_Circuiti.php"><b>Circuiti</b></a></li>
+					<li><a href="AR_Software/AR_Software.php"><b>Arduino IDE</b></a></li>
+					<li><a href="AR_Porta/AR_Porta.php"><b>Porta seriale</b></a></li>
+<!--					<li><a href="AR_Attuatori/AR_Attuatori.php"><b>Attuatori [...]</b></a></li>
+					<li><a href="AR_Sensori/AR_Sensori.php"><b>Sensori [...]</b></a></li>	-->	
+				</ul>
+				<!--<h3><a name="processing">Processing</a></h3>
+				<ul>
+					<li><a href="PR_Intro/PR_Intro.php"><b>Intro</b></a></li>
+					<li><a href="PR_Display/PR_Display.php"><b>Display</b></a></li>
+					<li><a href="PR_Shapes2D/PR_Shapes2D.php"><b>Shapes 2D</b></a></li> 
+				</ul>
+  			    <a href="Esercizi_vari/Esercizi_vari.php"><h3>Esercizi vari</h3></a>	
+				<h3><a name="max">Max</a></h3>
+				<ul>
+					<li><a href="Max_intro/Max_intro.php"><b>Intro</b></a></li>
+				</ul>
+                <p>Domande, suggerimenti o altro: <a href="mailto:anvigaan@gmail.com">anvigaan@gmail.com</a></p>-->	
+			</div>
+		</main>
+		
+		<nav id="nav">
+			<div class="innertube">
+				<img src="cage.gif" width="99%" alt="image not found">
+				<img src="barcode.gif" width="99%" alt="image not found">
+				<ul>
+					<li><a href="#home"><b>Home</b></a></li>
+					<p></p>
+					<li><a href="#appunti">Appunti</a></li>
+					<li><a href="#SC">SuperCollider</a></li>
+					<li><a href="#arduino">Arduino</a></li>
+					<!--<li><a href="#processing">Processing</a></li>
+					<li><a href="#max">Max</a></li>-->
+				</ul>
+			</div>
+		</nav>	
+	</body>
+</html>
